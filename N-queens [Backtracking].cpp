79bf8136db5364baace9444b711652d721d37dce
Jeon,junyeop <john40644064@gmail.com>
2019-04-27 17:55:29 -0500

    N-queens [Backtracking] 2019/04/27
    Backtracking

---
+++ N-queens [Backtracking].cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, result;
+
+int a[3][25];
+
+void solution(int i) {
+	if (i == n) {
+		result++;
+		
+	}
+	for (int j = 0; j < n; j++) {
+
+		if (a[0][j] || a[1][i + j] || a[2][n - 1 + i - j]) 
+			continue;
+		
+		a[0][j] = a[1][i + j] = a[2][n - 1 + i - j] = 1;
+		solution(i + 1);
+		a[0][j] = a[1][i + j] = a[2][n - 1 + i - j] = 0;
+
+	}
+
+}
+
+int main() {
+
+	cin >> n;
+	solution(0);
+	cout << result;
+
+	return 0;
+}
